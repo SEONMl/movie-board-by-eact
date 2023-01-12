779b3857aaf97db3e2bb8162889cf370dc373a18
SEONMl <sm000117@naver.com>
2023-01-12 23:13:05 +0900

    feat: 함수형 컴포넌트로 state 사용하기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div className='App'/>;
+  const [isLoading, setLoading]= useState(true);
+  const [movies, setMovie] = useState([]);
+
+  
+  useEffect(() =>{
+    setTimeout(()=>{
+      setLoading({isLoading:false});
+    },3000);
+    console.log("componentDidMount [Funtional Component]");
+  },[]);
+
+  return (
+    <div className='App'>
+      {isLoading? 'Loading...' : 'We are ready'}
+    </div>);
 }
 
 export default App;
