8d6f13319f269be1a846cb432c15c538403c9d1f
SEONMl <sm000117@naver.com>
2023-01-13 00:03:17 +0900

    feat: axios로 Movie 데이터 받아오는 기능 추가

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from "react";
+import axios  from 'axios';
 
 function App() {
   const [isLoading, setLoading]= useState(true);
   const [movies, setMovie] = useState([]);
 
-  
   useEffect(() =>{
-    setTimeout(()=>{
-      setLoading({isLoading:false});
-    },3000);
-    console.log("componentDidMount [Funtional Component]");
-  },[]);
-
+    async function getMovie() {
+      await axios
+      .get('https://yts.mx/api/v2/list_movies.json')
+      .then(data => setMovie(data.data.data.movies));
+      setLoading(false);
+    }
+    getMovie();
+    console.log(movies);
+    },[]);
+    
   return (
     <div className='App'>
       {isLoading? 'Loading...' : 'We are ready'}
