6b7956a74312a085b92790034363832bf47fe82b
SEONMl <sm000117@naver.com>
2023-01-13 13:35:52 +0900

    feat: API로 받아온 데이터 출력하기

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,24 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import axios  from 'axios';
+import Movie from './Component/Movie.js';
 
 function App() {
   const [isLoading, setLoading]= useState(true);
-  const [movies, setMovie] = useState([]);
+  const [movies, setMovie] = useState();
 
-  useEffect(() =>{
-    async function getMovie() {
-      await axios
-      .get('https://yts.mx/api/v2/list_movies.json')
-      .then(data => setMovie(data.data.data.movies));
+  async function getMovie() {
+    await axios
+    .get('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+    .then(res=> {
+      setMovie(res.data.data.movies);
       setLoading(false);
-    }
+    });
+  }
+  useEffect(() =>{
     getMovie();
-    console.log(movies);
     },[]);
-    
+
   return (
     <div className='App'>
-      {isLoading? 'Loading...' : 'We are ready'}
+      {isLoading
+      ? 'Loading...'
+      : movies.map((movie)=>{
+        return (
+          <Movie
+          key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.poster}
+          />
+        );
+      })}
     </div>);
 }
 
