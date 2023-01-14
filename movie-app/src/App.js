0496d90f3507bf0bfa09c76edf0a0b0c31c7333c
SEONMl <sm000117@naver.com>
2023-01-14 14:39:36 +0900

    fix: <Routes> 태그 추가

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Home from './routes/Home'
 import './CSS/App.css';
-import {HashRouter, Route} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 import About from './routes/About';
 import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
     <HashRouter>
       <Navigation />
-      <Route path='/' exact={true} component={Home} />
-      <Route path="/about" component={About} />
+      <Routes>
+        <Route path='/' exact={true} component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/movie-detail" component={Detail} />
+      </Routes>
     </HashRouter>
   );
 }
