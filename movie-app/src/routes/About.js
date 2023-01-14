9bd0ce47a2df89ba89707e147e0c8aa2212f7d7d
SEONMl <sm000117@naver.com>
2023-01-14 14:18:25 +0900

    feat: 소개 페이지 css 적용

--- movie-app/src/routes/About.js
+++ movie-app/src/routes/About.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function About(){
-    return <span>About this page: I built it because I love movies.</span>;
+    return (
+        <div className="abotu__container">
+            <span>About this page: I built it because I love movies.</span>;
+        </div>
+    );
 }
 
 export default About;
